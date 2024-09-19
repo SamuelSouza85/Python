abbed1b31dfdf2d000320ca4496b438fbfd51436
SamuelSouza85 <samueldesouza859@gmail.com>
2024-09-19 18:23:10 -0300

    Primeiro commit

---
+++ Desafio02.py
@@ -0,0 +1,5 @@
+dia = input ('Qual dia vc nasceu?')
+mês = input ('Em qual mês vc nasceu?')
+ano = input ('Em que ano vc nasceu?')
+
+print('Você nasceu no dia ' , dia, 'de' , mês, 'de' ,ano , '. Correto?'   )
